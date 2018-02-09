5c12420782c22b3e0fdb551d622295cba0562183
Joakim Mattsson <{username}@users.noreply.github.com>
2018-02-09 15:07:57 +0100

    testfil uppdaterad

--- TestFil.java
+++ TestFil.java
@@ -6,6 +6,8 @@ public class TestFil {
 
     SLCWithGet<Integer> list;
     SplayWithGet<Integer> splay;
+    SplayWithGet<String> splayString;
+    AVLwithGet<Integer> avlTree;
     Random rand = new Random();
 
     Integer lastAdded;
@@ -20,7 +22,9 @@ public class TestFil {
 
     public TestFil() {
         //testSLC();
-        testSplay();
+        //testSplay();
+        //testSplayString();
+        //testAvl();
 
     }
 
@@ -51,6 +55,21 @@ public class TestFil {
         splay.add(46);
         splay.add(18);
         splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
+        splay.add(49);
 
         lastAdded = 64;
 
@@ -64,12 +83,51 @@ public class TestFil {
 
     }
 
+    private void testSplayString() {
+        splayString = new SplayWithGet<>();
+
+        splayString.add("hej");
+        splayString.add("heja");
+        splayString.add("hejb");
+        splayString.add("hejd");
+        splayString.add("hejaa");
+        splayString.add("he");
+        splayString.add("heeeej");
+        splayString.add("hejab");
+        splayString.add("hejolk");
+        splayString.add("hejtr");
+        splayString.add("haa");
+        splayString.add("htgf");
+
+        System.out.println(splayString.toString());
+
+        splayString.get("he");
+
+        System.out.println(splayString.toString());
+
+
+    }
+
+    private void testAvl() {
+        avlTree = new AVLwithGet<>();
+
+        fillWithData(avlTree);
+
+        System.out.println(avlTree.toString());
+
+        avlTree.get(lastAdded);
+
+        System.out.println(avlTree.toString());
+
+    }
+
     private void fillWithData(Collection collection) {
         Integer some = 0;
         for (int i = 0; i < 10; i++) {
             int n = rand.nextInt(100);
 
             collection.add(n);
+
             some = n;
         }
         lastAdded = some;
