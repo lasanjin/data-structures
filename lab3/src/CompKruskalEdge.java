4d66c31b501f3816397eebc5ed777d3ace57a22d
root <root@DESKTOP-8UTTAON.localdomain>
2018-02-19 16:14:25 +0100

    mst

---
+++ lab3/src/CompKruskalEdge.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class CompKruskalEdge {
+
+    private CompKruskalEdge() {
+
+    }
+
+    public static <E extends Edge> LinkedList getKruskalGraph(int nEdge, Comparator comparator, List<E>[] graph) {
+        PriorityQueue pq = new PriorityQueue(nEdge, comparator);
+        List<E>[] mst = new List[nEdge];
+
+        for (List l : graph) {
+            pq.addAll(l);
+        }
+
+        while (!pq.isEmpty()) {
+            E edge = (E) pq.poll();
+
+            if (mst[edge.from] != mst[edge.to]) {
+                int sizeTo = mst[edge.to].size();
+                int sizeFrom = mst[edge.from].size();
+
+                if (sizeTo < sizeFrom) {
+                    mergeLists(mst, edge.to, edge.from);
+                } else {
+                    mergeLists(mst, edge.from, edge.to);
+                }
+
+                mst[edge.to] = mst[edge.from];
+
+            } else {
+                mst[edge.from].add(edge);
+            }
+        }
+
+        return null;
+    }
+
+    private static <E extends Edge> void mergeLists(List<E>[] mst, int small, int large) {
+        List<E> temp = mst[small];
+
+        if (temp.size() < 1) {
+            mst[small] = mst[large];
+            return;
+        }
+
+        int count = 0;
+        for (int i = 0; i < mst.length; i++) {
+            if (mst[i] == temp) {
+                mst[i] = mst[large];
+                mst[large].add(temp.get(count++));
+            }
+        }
+
+        //mst[large].addAll(temp);
+        // gör detta för att få bort en hel n iteration
+    }
+}
--- lab3/src/DirectedGraph.java
+++ lab3/src/DirectedGraph.java
@@ -3,39 +3,54 @@ import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
 
-	private int numV;
-	private List<E>[] edges;
+    private int numV;
+    private List<E>[] edges;
 
 
-	public DirectedGraph(int noOfNodes) {
-		edges = new List[noOfNodes];
-		numV = noOfNodes;
+    public DirectedGraph(int nNode) {
+        edges = new List[nNode];
+        numV = nNode;
 
-	}
+    }
 
-	public void addEdge(E e) {
-		if (e == null) {
-			throw new NullPointerException();
-		}
-		if (e.from < 0 || e.from <= numV) {
-			throw new IndexOutOfBoundsException();
-		}
-		if (edges[e.from].contains(e)) {
-			return;
-		}
+    public void addEdge(E e) {
+        if (e == null) {
+            throw new NullPointerException();
+        }
+        if (e.from < 0 || e.from <= numV) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (edges[e.from].contains(e)) {
+            return;
+        }
 
-		edges[e.from].add(e);
+        edges[e.from].add(e);
 
-	}
+    }
 
 
-	public Iterator<E> shortestPath(int from, int to) {
-		return null;
-	}
-		
-	public Iterator<E> minimumSpanningTree() {
-		return null;
-	}
+    public Iterator<E> shortestPath(int from, int to) {
+        return null;
+    }
+
+    public Iterator<E> minimumSpanningTree() {
+
+        return null;
+    }
+
+
+
+    //TODO Fråga hur noga vi ska vara med chipsen.
+    private static class compareEdge implements Comparator {
+
+        @Override
+        public int compare(Object o1, Object o2) {
+            double w1 = ((Edge) o1).getWeight();
+            double w2 = ((Edge) o2).getWeight();
+
+            return Double.compare(w1, w2);
+        }
+    }
 
 }
   
